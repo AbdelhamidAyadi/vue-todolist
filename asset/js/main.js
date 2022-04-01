655554ade72446cb311fdd6015248b32ca2c7583
AbdelhamidAyadi <Abdelhamid2002ayadi@gmail.com>
2022-04-01 23:44:48 +0200

    Commit

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,10 +1,28 @@
 var app = new Vue({
     el: '#root',
     data: {
-       
+        newTask: '',
+        toDoList: [
+            
+        ]
 
     },
     methods: {
-       
+        addTask: function() {
+            this.toDoList.push({
+                text: this.newTask,
+                done: false
+            },)
+            this.newTask = "";
+        },
+
+        removeTask: function(index) {
+             this.toDoList.splice(index, 1)
+        },
+        test: function(index) {
+            this.toDoList[index] = done = true
+       },
+
+
     }
 });
